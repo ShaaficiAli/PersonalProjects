9d78fe7fd82959d3e68cf91214497b00844bc292
Shaafici Ali <alishaafici@gmail.com>
2016-11-03 11:37:43 -0400

    added doc string and big o notation for binary search function

--- binary-search-function.cpp
+++ binary-search-function.cpp
@@ -2,6 +2,13 @@
 using namespace std; 
 int binary_search(int list[],int size,int number)
 {
+	/*
+	(list,int,int)-->int
+	This program finds out if a number is inside a list
+	Precondition: The list has to already be sorted
+	
+	O(log2(n))
+	*/
 	int b=0;
 	int e=size-1;
 	int mid;
@@ -26,13 +33,15 @@ int binary_search(int list[],int size,int number)
 }
 int main()
 {
-	int sortedlist[15]={1,2,3,4,5,6,7,14,16,18,21,24,36,37};
+	int sortedlist[]={1,2,3,4,5,6,7,14,16,18,21,24,36,37};
 	int number_being_searchedfor;
+	int size=sizeof(sortedlist)/4;
 	cout<<"Which number are You looking for from our list:";
 	cin>> number_being_searchedfor;
-	int answer=binary_search(sortedlist,14,number_being_searchedfor);
+	int answer=binary_search(sortedlist,size,number_being_searchedfor);
 	
-	if(answer!=-1){
+	if(answer!=-1)
+	{
 		cout<<"The number in the "<<answer<<"th positition"<<endl;
 	}
 	else
---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int list[]={1,2,3,4,5,6,7,14,16,18,21,24,36,37};
+	cout<<sizeof(15)<<" "<<sizeof(list)/4<<endl;
+	if(sizeof(list)/4==14)
+	{
+		cout<<"your right";
+	}
+}
