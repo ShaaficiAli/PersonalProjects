0261ef51a4e09dc88c3dd51b96cddb37374517b2
unknown <alishaafici@gmail.com>
2018-12-31 18:43:16 -0500

    Added comments to better explain functions

--- RedditAnalysis/RedditDatabase.py
+++ RedditAnalysis/RedditDatabase.py
@@ -5,6 +5,12 @@ from BotInfo import Bot
 import datetime
 import numpy as np
 def getSubredditUserPostData(SubredditName,dbFileName,maxRedditor=25,maxSubmissions=500):
+    """
+        This function retrieves post history of redditors who post in SubredditName and stores the post title,which subreddit it was posted in, the utc date it was posted,
+        the amount of likes it has and the name of the redditor. If a table for SubredditName has not been intialized and table will be created that has its name.
+        Only Unique posts are retrieved.If the db file from dbFileName is not initialized then it will be created for you called dbFileName.
+    """
+    #The Bot used is stored in another class file. The File is BotInfo use it if you want.
     reddit = praw.Reddit(client_id=Bot.client_id,
                          client_secret=Bot.secret,
                          username=Bot.UserName,
@@ -13,6 +19,7 @@ def getSubredditUserPostData(SubredditName,dbFileName,maxRedditor=25,maxSubmissi
                          )
     connection = sqlite3.connect(dbFileName)
     cursor = connection.cursor()
+    #If the table is not in the dbFileName then create it
     if cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"+SubredditName+
                       "'").fetchall()==[]:
         print("CREATING TABLE "+SubredditName)
@@ -20,6 +27,7 @@ def getSubredditUserPostData(SubredditName,dbFileName,maxRedditor=25,maxSubmissi
     
     for submission in reddit.subreddit(SubredditName).hot(limit=maxSubmissions):
         redditor = submission.author
+        #searching through the history of submitters from the subreddit given
         for post in redditor.submissions.new(limit=maxRedditor):
             date = int(post.created_utc)
             karma = post.score
@@ -28,20 +36,22 @@ def getSubredditUserPostData(SubredditName,dbFileName,maxRedditor=25,maxSubmissi
             name = redditor.name
             item = (sub,name,title,date,karma,)
             sqlStatement = "SELECT subreddit, username, title, date FROM '"+SubredditName+"' WHERE subreddit = ? and username = ? and title = ? and date = ?"
-            print(sqlStatement)
+            #if this particular post has not already been added to the subreddit then add it
             if cursor.execute(sqlStatement,(sub,name,title,date)).fetchall()==[]:
-                print("Sucess")
                 cursor.execute("INSERT INTO '"+SubredditName+"' (subreddit,username,title,date,karma) VALUES(?,?,?,?,?)",item)
     connection.commit()
     connection.close()
     
 def SubredditRedditorsPostDistributions(SubredditName,dbFileName):
+    """
+        This function returns a list of dates in the table given by SubredditName and changes the date item in each query item to how many days that post was before the most recent post.
+        It also returns the name of the subreddit and how many days the entire table spans. 
+    """
     connection = sqlite3.connect(dbFileName)
     cursor = connection.cursor()
     Maximum = cursor.execute("SELECT MAX(date) FROM '"+SubredditName+"'").fetchone()[0]
     Minimum = cursor.execute("SELECT MIN(date) FROM '"+SubredditName+"'").fetchone()[0]
     duration = (datetime.date.fromtimestamp(Maximum)-datetime.date.fromtimestamp(Minimum)).days
-    
     data = []
     for datapoint in cursor.execute("SELECT date FROM '"+SubredditName+"' ORDER BY date DESC"):
         utc_time = datapoint[0]
@@ -50,6 +60,10 @@ def SubredditRedditorsPostDistributions(SubredditName,dbFileName):
     return [data,SubredditName,duration]
 
 def displayHistogram(*args):
+    """
+        Creates a histogram from the data given by SubreddRedditorsPostDistributions(). If multiple sets of data are given then the histogram will compare and contrast those
+        datasets and provide a legend for the color of each.The title will reflect which subreddits are given
+    """
     subredditsInvolved = ""
     moreThanOne = len(args)>1
     for data in args:
@@ -66,7 +80,9 @@ def displayHistogram(*args):
     plt.xlabel("Days before newest post")
     plt.ylabel("Cumulative distribution function of posts prior to most recent")
     plt.show()
-
+"""
+if Post frequency froms users from 'The_Donald' and 'all' want to be put side by side for analysis then this is the way to use the code to do so. The db file is called RedditUsers.db
+"""
 getSubredditUserPostData("The_Donald","RedditUsers.db")
 getSubredditUserPostData("all","RedditUsers.db")
 data1 = SubredditRedditorsPostDistributions("The_Donald","RedditUsers.db")
